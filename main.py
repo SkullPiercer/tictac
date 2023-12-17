fe72964e2d0a4d86db78010393fc8da4c7a67cfa
Александр Денисов <akatsuki_sanya@vk.com>
2023-12-17 16:16:34 +0600

    Add construction if name == main

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-# Объявить класс.
-class Board:
-    # Инициализировать игровое поле - список списков с пробелами.
-    # Пробелы — это пустые клетки.
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
-    # Метод, который обрабатывает ходы игроков.
-    def make_move(self, row, col, player):
-        self.board[row][col] = player
+from gameparts import Board
 
-    # Метод, который отрисовывает игровое поле.
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
-
-# Создать игровое поле - объект класса Board.
-game = Board()
-# Отрисовать поле в терминале.
-game.display()
-# Разместить на поле символ по указанным координатам — сделать ход.
-game.make_move(2, 0, 'X')
-print('Ход сделан!')
-# Перерисовать поле с учётом сделанного хода.
-game.display()
+# Всё, что ниже этой инструкции, не будет импортироваться,
+# но будет выполняться при запуске файла game.py.
+if __name__ == '__main__':
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display()
